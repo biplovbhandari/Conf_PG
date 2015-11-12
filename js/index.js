22b5a21922eb4c89d975a668bf3a775ea5c604ad
biplovbhandari <bionicbiplov45@gmail.com>
2015-11-12 17:13:27 +0700

    First Commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        var self = this;
+        self.triggerMainPage();
+    },
+
+    triggerMainPage() {
+
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
